e2793c6303d165c21ce8d453453593137d7c4e0f
student5 <student3@jh-mba158-19.d214.org>
2017-04-28 09:55:53 -0500

    added nodes and categories

--- AirHockey/GameScene.swift
+++ AirHockey/GameScene.swift
@@ -9,7 +9,21 @@
 import SpriteKit
 import GameplayKit
 
+let puckCategory: UInt32 = 0x1 << 0
+let bottomCategory: UInt32 = 0x1 << 1
+let topCategory: UInt32 = 0x1 << 2
+let leftCategory: UInt32 = 0x1 << 3
+let rightCategory: UInt32 = 0x1 << 4
+let paddleCategory: UInt32 = 0x1 << 5
+let goalCategory: UInt32 = 0x1 << 6
+
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var rightPaddle = SKSpriteNode()
+    var leftPaddle = SKSpriteNode()
+    var rightGoal = SKSpriteNode()
+    var leftGoal = SKSpriteNode()
+    var puck = SKSpriteNode()
     
     override func didMove(to view: SKView)
     {
