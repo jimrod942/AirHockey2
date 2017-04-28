56999424502527217080a3a9313baef874fd9297
student5 <student3@jh-mba158-17.d214.org>
2017-04-28 09:48:29 -0500

    Added border for gameScene.swift - JR

--- AirHockey/GameScene.swift
+++ AirHockey/GameScene.swift
@@ -12,9 +12,38 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+//        let border = SKPhysicsBody(edgeFrom: <#T##CGPoint#>, to: <#T##CGPoint#>)
+//        border.friction = 0
+//        self.physicsBody = border
+        let bottomLeft = CGPoint(x: frame.origin.x, y: frame.origin.y)
+        let bottomRight = CGPoint(x: -frame.origin.x, y: frame.origin.y)
+        let topLeft = CGPoint(x: frame.origin.x, y: -462)
+        let topRight = CGPoint(x: -frame.origin.x, y: 462)
+        
+        let bottom = SKSpriteNode()
+        bottom.name = "bottom"
+        bottom.physicsBody = SKPhysicsBody(edgeFrom: bottomLeft, to: bottomRight)
+        
+        let left = SKSpriteNode()
+        left.name = "left"
+        left.physicsBody = SKPhysicsBody(edgeFrom: bottomLeft, to: topLeft)
+        
+        let top = SKSpriteNode()
+        top.name = "top"
+        top.physicsBody = SKPhysicsBody(edgeFrom: topLeft, to: topRight)
+        
+        let right = SKSpriteNode()
+        right.name = "right"
+        right.physicsBody = SKPhysicsBody(edgeFrom: topRight, to: bottomRight)
+        
+        addChild(bottom)
+        addChild(left)
+        addChild(top)
+        addChild(right)
+
     }
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+
     }
 }
